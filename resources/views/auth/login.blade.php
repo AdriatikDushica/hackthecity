d288d0a0ea10eab59db9fff6b85262a0279bc6f1
Adriatik Dushica <adriatik.dushica@gmail.com>
2016-08-26 19:58:39 +0200

    localization and views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,7 +42,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember"> Remember Me
+                                        <input type="checkbox" name="remember"> ricorda accesso
                                     </label>
                                 </div>
                             </div>
