b627d69a21ef59de086d6b1ab3a87b67ba2de93a
Adriatik Dushica <adriatik.dushica@gmail.com>
2016-08-27 19:35:12 +0200

    changes

--- resources/views/auth/external.blade.php
+++ resources/views/auth/external.blade.php
@@ -3,6 +3,5 @@
     <div class="panel-body">
         <a href="{{ url('auth/google') }}" class="btn btn-default btn-block btn-danger"><i class="fa fa-google" aria-hidden="true"></i> Google</a>
         <a href="{{ url('auth/twitter') }}" class="btn btn-default btn-block btn-primary"><i class="fa fa-twitter" aria-hidden="true"></i> Twitter</a>
-        <div class="btn btn-default btn-block"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</div>
     </div>
 </div>
